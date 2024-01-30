f7ffe649abbe959927c26d9a87b3b425031bd6be
Soczó <lonsdale201@hotmail.com>
2024-01-31 00:15:28 +0100

    bump 2.0

--- my-profile-menu.php
+++ my-profile-menu.php
@@ -3,7 +3,7 @@
 Plugin Name: HelloWP! | My Profile Menu
 Plugin URI: https://github.com/Lonsdale201/wp-mymenu
 Description: MyAccount Dropdown menu and extras.
-Version: 1.7
+Version: 2.0
 Author: Soczó Kristóf
 Author URI: https://hellowp.io/hu/
 */
