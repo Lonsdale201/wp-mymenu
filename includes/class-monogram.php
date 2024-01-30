7f002d27787a8b715b6cf3192ca493663d4218cf
Soczó <lonsdale201@hotmail.com>
2024-01-31 00:15:50 +0100

    bump2.0

--- includes/class-monogram.php
+++ includes/class-monogram.php
@@ -37,26 +37,35 @@ class Monogram {
         if (is_user_logged_in()) {
             $user = wp_get_current_user();
             $monogram = get_user_meta($user->ID, 'hw_monogram', true);
-
+    
             if (empty($monogram)) {
                 $monogram = $this->generate_monogram($user);
             }
-
+    
+            $before_content = get_option('mymenu_before_dropdown_content');
+            $after_content = get_option('mymenu_after_dropdown_content');
+    
             ob_start();
+            if (!empty($before_content)) {
+                echo do_shortcode($before_content);
+            }
             include plugin_dir_path( __FILE__ ) . '../templates/monogram-template.php';
+            if (!empty($after_content)) {
+                echo do_shortcode($after_content);
+            }
             return ob_get_clean();
         } else {
             $redirect_url = get_option('mymenu_redirect_url');
             $logged_out_text = get_option('mymenu_text_field', 'Bejelentkezés / Regisztráció');
-    
+        
             if (!$redirect_url) {
-                
                 $redirect_url = site_url('/fiokom');
             }
-    
+        
             return '<a class="logged-out-button" href="' . esc_url($redirect_url) . '">' . esc_html($logged_out_text) . '</a>';
         }
     }
+    
 
     public function handle_user_register($user_id) {
         $user = get_userdata($user_id);
